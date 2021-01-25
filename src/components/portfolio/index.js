3aea47f61f94d374b38b316c1138bda126c4cd0e
jonathan <jonathanms30@gmail.com>
2021-01-24 21:02:31 -0300

    created portfolio card

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Portfolio = () => (
-    <>
-    </>
-  );
-  
-  export default Portfolio;
+    <section className="portfolio" id="portfolio">
+        <div className="container">
+            <h2 className="about-title">Portfolio</h2>
+        </div>
+        <div className="container">
+            <div className="portfolio-card"></div>
+        </div>
+    </section>
+);
+
+export default Portfolio;
